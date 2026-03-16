fc4a2c64a028de520b8b4bfba2d1c9e2e080a303
shimseunggi <rosexpedition0506@gmail.com>
2026-03-16 15:21:24 +0900

    news update

--- data/site-data.js
+++ data/site-data.js
@@ -28,12 +28,12 @@ window.NURA_DATA = {
 
   newsItems: [
     {
-      date: "2023.02.21",
-      title: "전국대학교로켓연합회 명예회원증 받는 윤석열 대통령",
+      date: "2024.07.08",
+      title: "GIST 우주공학 동아리, 전국대학 우수로켓논문상 수상",
       summary:
-        "우주경제 개척자와의 대화 행사에서 전국대학교로켓연합회 관련 장면을 다룬 기사입니다.",
+        "광주과학기술원(GIST) 우주공학 학생 동아리, 행성탐사연구소가 '2024 한국추진공학회 춘계학술대회'에서 '전국대학교로켓연합회 우수발표논문상'을 수상했습니다.",
       image: "source/news/news1.jpg",
-      href: "https://www.yna.co.kr/view/PYH20230221128900013"
+      href: "https://news.kbs.co.kr/news/pc/view/view.do?ncd=8006132&ref=A"
     },
     {
       date: "2022.06.11",
@@ -47,7 +47,7 @@ window.NURA_DATA = {
       date: "2024.01.05",
       title: "부산대학교 항공우주공학과 동아리 PRO, 전국항공우주과학경진대회 대상·금상 수상",
       summary:
-        "전국항공우주과학경진대회에서 부산대 항공우주공학과 동아리가 금상을 수상한 기사입니다.",
+        "전국항공우주과학경진대회에서 부산대 항공우주공학과 동아리가 대상 및 금상을 수상한 기사입니다.",
       image: "source/news/news3.jpg",
       href: "https://www.lecturernews.com/news/articleView.html?idxno=143887"
     }
