9c4637d8fd6c81038a0cf221e4f713113a7d4fd2
shimseunggi <rosexpedition0506@gmail.com>
2026-04-01 15:47:47 +0900

    최적화2

--- data/site-data.js
+++ data/site-data.js
@@ -28,13 +28,6 @@ window.NURA_DATA = {
 
   newsItems: [
     {
-      date: "2024.07.08",
-      title: "GIST 우주공학 동아리, 전국대학 우수로켓논문상 수상",
-      summary: "광주과학기술원(GIST) 우주공학 학생 동아리, 행성탐사연구소가 '2024 한국추진공학회 춘계학술대회'에서 '전국대학교로켓연합회 우수발표논문상'을 수상했습니다.",
-      image: "source/news/news1.jpg",
-      href: "https://news.kbs.co.kr/news/pc/view/view.do?ncd=8006132&ref=A"
-    },
-    {
       date: "2022.06.11",
       title: "[한국 우주시대 본격 개막]로켓 직접 깎고 다듬고, 우주산업 혁명 벼르는 ‘우주키즈'",
       summary: "한국의 우주산업을 이끌 우주키즈들의 마음 한 켠엔 저마다의 로켓이 자리 잡고 있다.",
@@ -47,6 +40,13 @@ window.NURA_DATA = {
       summary: "전국항공우주과학경진대회에서 부산대 항공우주공학과 동아리가 대상 및 금상을 수상한 기사입니다.",
       image: "source/news/news3.jpg",
       href: "https://www.lecturernews.com/news/articleView.html?idxno=143887"
+    },
+        {
+      date: "2024.07.08",
+      title: "GIST 우주공학 동아리, 전국대학 우수로켓논문상 수상",
+      summary: "광주과학기술원(GIST) 우주공학 학생 동아리, 행성탐사연구소가 '2024 한국추진공학회 춘계학술대회'에서 '전국대학교로켓연합회 우수발표논문상'을 수상했습니다.",
+      image: "source/news/news1.jpg",
+      href: "https://news.kbs.co.kr/news/pc/view/view.do?ncd=8006132&ref=A"
     }
   ],
   
